9ba7f7cff79ab58eb3f36dd9afe8a25e6f8eba9b
wrlu <wrlu.cn@outlook.com>
2024-10-16 16:51:19 +0800

    Fix compile.

--- app/src/main/java/com/wrlus/virtcam/MainActivity.java
+++ app/src/main/java/com/wrlus/virtcam/MainActivity.java
@@ -9,6 +9,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
